77c90cb94488da0150b1c456923331059e3a7154
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2024-11-28 22:10:39 +0000

    ajout d'une fonctionnalité

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import Header from "./components/header/Header.jsx";
-import Home from "./components/home/Home.jsx";
-import Management from "./components/management/Management.jsx";
-import Analytic from "./components/analytic/Analytic.jsx";
-import Professional from "./components/professional/Professional.jsx";
-import QRCode from "./components/qrcode/QRCode.jsx";
-import Footer from "./components/footer/Footer.jsx";
-
 function App() {
 
   return (
-      <>
-          {/*<Header></Header>*/}
-          {/*<Home></Home>*/}
-          {/*<Management></Management>*/}
-          {/*<Analytic></Analytic>*/}
-          {/*<Professional></Professional>*/}
-          {/*<QRCode></QRCode>*/}
-          {/*<Footer></Footer>*/}
-      </>
+      <div>
+
+      </div>
   )
 }
 
